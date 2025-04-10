566b9821738fed66b87e034c54a80aff8595adf7
Stanislav Povkh <s.povkh@astounddigital.com>
2025-04-11 02:43:30 +0300

    Auth

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,23 +1,29 @@
 import { z } from 'zod'
 import { OAuth2Client } from 'google-auth-library'
 
+const clientId =
+  '64929895773-qrit0c1f2rljcof7jjogb7ik6vg7gdjh.apps.googleusercontent.com'
+const clientSecret = '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'
+
 const bodySchema = z.object({
-  token: z.string()
+  token: z.string(),
 })
 
 export default defineEventHandler(async (event) => {
   const { token } = await readValidatedBody(event, bodySchema.parse)
 
   try {
-    const client = new OAuth2Client()
-    const ticket = await client.verifyIdToken({ idToken: token })
+    const client = new OAuth2Client({ clientId, clientSecret })
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: clientId,
+    })
     const payload = ticket.getPayload()
 
-    await setUserSession(event, { user: payload})
+    await setUserSession(event, { user: payload })
 
     return { ok: true, payload }
   } catch (error) {
     return { ok: false, error }
   }
-
 })
