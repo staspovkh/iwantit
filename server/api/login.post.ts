bbb5c3e95b7f7683fc7d0b236c08d1a5254f3543
Stanislav Povkh <s.povkh@astounddigital.com>
2025-04-11 18:08:33 +0300

    Auth2

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { OAuth2Client } from 'google-auth-library'
 import type { User } from '~/types'
 
-const { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_CLIENT_SECRET } = process.env
+const { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_CLIENT_SECRET } = process.env
 
 const bodySchema = z.object({
   token: z.string(),
@@ -12,52 +12,34 @@ const bodySchema = z.object({
 export default defineEventHandler(async (event) => {
   const { token } = await readValidatedBody(event, bodySchema.parse)
 
-  try {
-    if (token) {
-      const client = new OAuth2Client({
-        clientId: GOOGLE_AUTH_CLIENT_ID,
-        clientSecret: GOOGLE_AUTH_CLIENT_SECRET,
-        redirectUri: 'postmessage',
-      })
-
+  if (token) {
+    const client = new OAuth2Client({
+      clientId: GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: GOOGLE_OAUTH_CLIENT_SECRET,
+    })
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_OAUTH_CLIENT_ID,
+    })
+    const payload = ticket.getPayload()
+    if (payload?.email) {
+      const user: User = {
+        id: payload.sub,
+        email: payload.email,
+        name: payload.name,
+        picture: payload.picture,
+        given_name: payload.given_name,
+        family_name: payload.family_name,
+      }
+      await setUserSession(event, { user })
       return {
         ok: true,
-        payload: {
-          token,
-        },
+        payload: user,
       }
     }
-    // const client = new OAuth2Client()
-    // const ticket = await client.verifyIdToken({
-    //   idToken: token,
-    //   audience: clientId,
-    // })
-    // const payload = ticket.getPayload()
-
-    // if (payload?.email) {
-    //   const user: User = {
-    //     id: payload.sub,
-    //     email: payload.email,
-    //     name: payload.name,
-    //     picture: payload.picture,
-    //     given_name: payload.given_name,
-    //     family_name: payload.family_name,
-    //   }
-
-    //   return {
-    //     ok: true,
-    //     payload: user,
-    //   }
-    // }
-
-    return {
-      ok: false,
-      error: 'Invalid token',
-    }
-  } catch (error) {
-    return {
-      ok: false,
-      error,
-    }
+  }
+  return {
+    ok: false,
+    error: 'Invalid token',
   }
 })
